696ee4d936316d3c1ecaf893bad69eadace09487
Michael Black <michael.black@ice.com>
2025-11-05 11:21:20 -0700

    free for all teams and fix player ui to not show everyone's ui

--- mods/SorensonSnipes/SorensonSnipes.ts
+++ mods/SorensonSnipes/SorensonSnipes.ts
@@ -97,86 +97,123 @@ class PlayerUI {
     SCORE_BAR_WIDTH: number = 400;
 
     constructor(player: mod.Player) {
-        this.rootWidget = ParseUI({
-            name: "root_container",
-            type: "Container",
-            size: [this.SCORE_BAR_WIDTH, 200],
-            position: [0, 100],
-            anchor: mod.UIAnchor.TopCenter,
-            bgFill: mod.UIBgFill.None,
-            bgColor: COLORS.black,
-            bgAlpha: 0.8
-        });
+       const rootName: string = "ui_root_" + player;
+       const playerPlacementName: string = "ui_player_placement_" + player;
+       const enemyPlacementName: string = "ui_enemy_placement_" + player;
+       const progressBarName: string = "ui_progress_bar_" + player;
+       const playerIndicatorName: string = "ui_player_indicator_" + player;
+       const enemyIndicatorName: string = "ui_enemy_indicator_" + player;
+
+        mod.AddUIContainer(
+            rootName, // name
+            mod.CreateVector(0, 100, 0), // position
+            mod.CreateVector(this.SCORE_BAR_WIDTH, 200, 0), // size
+            mod.UIAnchor.TopCenter, // anchor
+            mod.GetUIRoot(), // parent
+            true, // visible
+            0, // padding
+            COLORS.black, // bgColor
+            0.8, // bgAlpha
+            mod.UIBgFill.None, // bgFill
+            player // player
+        );
+        this.rootWidget = mod.FindUIWidgetWithName(rootName);
         
         if (!this.rootWidget) return;
 
-        this.playerPlacementWidget = ParseUI({
-            name: "placement_player",
-            type: "Text",
-            parent: this.rootWidget,
-            textSize: 24,
-            position: [0, 0, 0],
-            size: [100, 50],
-            anchor: mod.UIAnchor.TopLeft,
-            textAnchor: mod.UIAnchor.CenterLeft,
-            bgAlpha: 0,
-            bgFill: mod.UIBgFill.Blur,
-            textLabel: mod.Message(mod.stringkeys.text_num_st_user, 1, player),
-            textColor: COLORS.blue
-        })
+        mod.AddUIText(
+            playerPlacementName, // name
+            mod.CreateVector(0, 0, 0), // position
+            mod.CreateVector(100, 50, 0), // size
+            mod.UIAnchor.TopLeft, // anchor
+            this.rootWidget, // parent
+            true, // visible
+            1, // padding
+            COLORS.blue, // bgColor
+            0.1, // bgAlpha
+            mod.UIBgFill.Blur, // bgFill
+            mod.Message(mod.stringkeys.text_num_st_user, 1, player), // message
+            24, // textSize
+            COLORS.blue, // textColor
+            1, // textAlpha
+            mod.UIAnchor.CenterLeft, // textAnchor
+            player
+        );
+        this.playerPlacementWidget = mod.FindUIWidgetWithName(playerPlacementName);
 
-        this.enemyPlacementWidget = ParseUI({
-            name: "placement_enemy",
-            type: "Text",
-            parent: this.rootWidget,
-            textSize: 24,
-            position: [0, 0, 0],
-            size: [100, 50],
-            anchor: mod.UIAnchor.TopRight,
-            textAnchor: mod.UIAnchor.CenterRight,
-            bgAlpha: 0,
-            bgFill: mod.UIBgFill.Blur,
-            textLabel: mod.Message(mod.stringkeys.text_num_nd_user, 2, player),
-            textColor: COLORS.red
-        })
-        
-        this.progressBarWidget = ParseUI({
-            name: "progress_bar",
-            type: "Container",
-            parent: this.rootWidget,
-            position: [0, 0, 0],
-            size: [this.SCORE_BAR_WIDTH, 50],
-            anchor: mod.UIAnchor.Center,
-            bgAlpha: 0,
-            bgFill: mod.UIBgFill.Solid,
-            bgColor: COLORS.gray
-        });
+        mod.AddUIText(
+            enemyPlacementName, // name
+            mod.CreateVector(0, 0, 0), // position
+            mod.CreateVector(100, 50, 0), // size
+            mod.UIAnchor.TopRight, // anchor
+            this.rootWidget, // parent
+            true, // visible
+            1, // padding
+            COLORS.red, // bgColor
+            0.1, // bgAlpha
+            mod.UIBgFill.Blur, // bgFill
+            mod.Message(mod.stringkeys.text_num_nd_user, 2, player), // message
+            24, // textSize
+            COLORS.red, // textColor
+            1, // textAlpha
+            mod.UIAnchor.CenterRight, // textAnchor
+            player
+        );
+        this.enemyPlacementWidget = mod.FindUIWidgetWithName(enemyPlacementName);
+
+        mod.AddUIContainer(
+            progressBarName, // name
+            mod.CreateVector(0, 0, 0), // position
+            mod.CreateVector(this.SCORE_BAR_WIDTH, 50, 0), // size
+            mod.UIAnchor.Center, // anchor
+            this.rootWidget, // parent
+            true, // visible
+            0, // padding
+            COLORS.gray, // bgColor
+            0.3, // bgAlpha
+            mod.UIBgFill.Blur, // bgFill
+            player // player
+        );
+        this.progressBarWidget = mod.FindUIWidgetWithName(progressBarName);
 
         if (!this.progressBarWidget) return;
         
-        this.playerIndicatorWidget = ParseUI({
-            name: "player_indicator",
-            type: "Image",
-            parent: this.progressBarWidget,
-            position: [0, 0, 0],
-            size: [50, 50],
-            anchor: mod.UIAnchor.CenterLeft,
-            bgAlpha: 0,
-            imageType: mod.UIImageType.CrownSolid, 
-            imageColor: COLORS.blue,
-        });
+        mod.AddUIImage(
+            playerIndicatorName, // name
+            mod.CreateVector(0, 0, 0), // position
+            mod.CreateVector(50, 50, 0), // size
+            mod.UIAnchor.CenterLeft, // anchor
+            this.progressBarWidget, //parent
+            true, // visible
+            0, // padding
+            mod.CreateVector(0, 0, 0), //bgColor
+            0, // bgAlpha
+            mod.UIBgFill.None, // bgFill
+            mod.UIImageType.CrownSolid, // imageType
+            COLORS.blue, // imageColor
+            1, // imageAlpha
+            player
+        )
+        this.playerIndicatorWidget = mod.FindUIWidgetWithName(playerIndicatorName);
+        
 
-        this.enemyIndicatorWidget = ParseUI({
-            name: "enemy_indicator",
-            type: "Image",
-            parent: this.progressBarWidget,
-            position: [0, 0, 0],
-            size: [50, 50],
-            anchor: mod.UIAnchor.CenterLeft,
-            bgAlpha: 0,
-            imageType: mod.UIImageType.CrownSolid, 
-            imageColor: COLORS.red,
-        });
+        mod.AddUIImage(
+            enemyIndicatorName, // name
+            mod.CreateVector(0, 0, 0), // position
+            mod.CreateVector(50, 50, 0), // size
+            mod.UIAnchor.CenterLeft, // anchor
+            this.progressBarWidget, //parent
+            true, // visible
+            0, // padding
+            mod.CreateVector(0, 0, 0), //bgColor
+            0, // bgAlpha
+            mod.UIBgFill.None, // bgFill
+            mod.UIImageType.CrownSolid, // imageType
+            COLORS.red, // imageColor
+            1, // imageAlpha
+            player
+        )
+        this.playerIndicatorWidget = mod.FindUIWidgetWithName(enemyIndicatorName);
     }
 
     updateScores(player: Player, enemy: Player, targetScore: number) {
@@ -221,9 +258,11 @@ export function OnGameModeStarted() {
     SCOREBOARD = new Scoreboard();
 }
 
+var NumTeams = 0;
 export function OnPlayerJoinGame(player: mod.Player) {
     const id = mod.GetObjId(player);
     PLAYERS[id] = new Player(player);
+    mod.SetTeam(player, mod.GetTeam(++NumTeams));
 }
 
 export function OnPlayerDied(
@@ -275,307 +314,4 @@ export function OnPlayerEarnedKillAssist(eventPlayer: mod.Player, eventOtherPlay
     const player = getPlayerFromModPlayer(eventPlayer);
     player.stats.assists++;
     SCOREBOARD.updatePlayerScoreboard(player);
-}
-
-// ****************************** PARSE UI HELPER ****************************** //
-type UIVector = mod.Vector | number[];
-
-interface UIParams {
-    name: string;
-    type: string;
-    position: any;
-    size: any;
-    anchor: mod.UIAnchor;
-    parent: mod.UIWidget;
-    visible: boolean;
-    textLabel: string;
-    textColor: UIVector;
-    textAlpha: number;
-    textSize: number;
-    textAnchor: mod.UIAnchor;
-    padding: number;
-    bgColor: UIVector;
-    bgAlpha: number;
-    bgFill: mod.UIBgFill;
-    imageType: mod.UIImageType;
-    imageColor: UIVector;
-    imageAlpha: number;
-    teamId?: mod.Team;
-    playerId?: mod.Player;
-    children?: any[];
-    buttonEnabled: boolean;
-    buttonColorBase: UIVector;
-    buttonAlphaBase: number;
-    buttonColorDisabled: UIVector;
-    buttonAlphaDisabled: number;
-    buttonColorPressed: UIVector;
-    buttonAlphaPressed: number;
-    buttonColorHover: UIVector;
-    buttonAlphaHover: number;
-    buttonColorFocused: UIVector;
-    buttonAlphaFocused: number;
-}
-
-function __asModVector(param: number[] | mod.Vector) {
-    if (Array.isArray(param)) return mod.CreateVector(param[0], param[1], param.length == 2 ? 0 : param[2]);
-    else return param;
-}
-
-function __asModMessage(param: string | mod.Message) {
-    if (typeof param === 'string') return mod.Message(param);
-    return param;
-}
-
-function __fillInDefaultArgs(params: UIParams) {
-    if (!params.hasOwnProperty('name')) params.name = '';
-    if (!params.hasOwnProperty('position')) params.position = mod.CreateVector(0, 0, 0);
-    if (!params.hasOwnProperty('size')) params.size = mod.CreateVector(100, 100, 0);
-    if (!params.hasOwnProperty('anchor')) params.anchor = mod.UIAnchor.TopLeft;
-    if (!params.hasOwnProperty('parent')) params.parent = mod.GetUIRoot();
-    if (!params.hasOwnProperty('visible')) params.visible = true;
-    if (!params.hasOwnProperty('padding')) params.padding = params.type == 'Container' ? 0 : 8;
-    if (!params.hasOwnProperty('bgColor')) params.bgColor = mod.CreateVector(0.25, 0.25, 0.25);
-    if (!params.hasOwnProperty('bgAlpha')) params.bgAlpha = 0.5;
-    if (!params.hasOwnProperty('bgFill')) params.bgFill = mod.UIBgFill.Solid;
-}
-
-function __setNameAndGetWidget(uniqueName: any, params: any) {
-    let widget = mod.FindUIWidgetWithName(uniqueName) as mod.UIWidget;
-    mod.SetUIWidgetName(widget, params.name);
-    return widget;
-}
-
-const __cUniqueName = '----uniquename----';
-
-function __addUIContainer(params: UIParams) {
-    __fillInDefaultArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIContainer(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            restrict
-        );
-    } else {
-        mod.AddUIContainer(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill
-        );
-    }
-    let widget = __setNameAndGetWidget(__cUniqueName, params);
-    if (params.children) {
-        params.children.forEach((childParams: any) => {
-            childParams.parent = widget;
-            __addUIWidget(childParams);
-        });
-    }
-    return widget;
-}
-
-function __fillInDefaultTextArgs(params: UIParams) {
-    if (!params.hasOwnProperty('textLabel')) params.textLabel = '';
-    if (!params.hasOwnProperty('textSize')) params.textSize = 0;
-    if (!params.hasOwnProperty('textColor')) params.textColor = mod.CreateVector(1, 1, 1);
-    if (!params.hasOwnProperty('textAlpha')) params.textAlpha = 1;
-    if (!params.hasOwnProperty('textAnchor')) params.textAnchor = mod.UIAnchor.CenterLeft;
-}
-
-function __addUIText(params: UIParams) {
-    __fillInDefaultArgs(params);
-    __fillInDefaultTextArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIText(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            __asModMessage(params.textLabel),
-            params.textSize,
-            __asModVector(params.textColor),
-            params.textAlpha,
-            params.textAnchor,
-            restrict
-        );
-    } else {
-        mod.AddUIText(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            __asModMessage(params.textLabel),
-            params.textSize,
-            __asModVector(params.textColor),
-            params.textAlpha,
-            params.textAnchor
-        );
-    }
-    return __setNameAndGetWidget(__cUniqueName, params);
-}
-
-function __fillInDefaultImageArgs(params: any) {
-    if (!params.hasOwnProperty('imageType')) params.imageType = mod.UIImageType.None;
-    if (!params.hasOwnProperty('imageColor')) params.imageColor = mod.CreateVector(1, 1, 1);
-    if (!params.hasOwnProperty('imageAlpha')) params.imageAlpha = 1;
-}
-
-function __addUIImage(params: UIParams) {
-    __fillInDefaultArgs(params);
-    __fillInDefaultImageArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIImage(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.imageType,
-            __asModVector(params.imageColor),
-            params.imageAlpha,
-            restrict
-        );
-    } else {
-        mod.AddUIImage(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.imageType,
-            __asModVector(params.imageColor),
-            params.imageAlpha
-        );
-    }
-    return __setNameAndGetWidget(__cUniqueName, params);
-}
-
-function __fillInDefaultArg(params: any, argName: any, defaultValue: any) {
-    if (!params.hasOwnProperty(argName)) params[argName] = defaultValue;
-}
-
-function __fillInDefaultButtonArgs(params: any) {
-    if (!params.hasOwnProperty('buttonEnabled')) params.buttonEnabled = true;
-    if (!params.hasOwnProperty('buttonColorBase')) params.buttonColorBase = mod.CreateVector(0.7, 0.7, 0.7);
-    if (!params.hasOwnProperty('buttonAlphaBase')) params.buttonAlphaBase = 1;
-    if (!params.hasOwnProperty('buttonColorDisabled')) params.buttonColorDisabled = mod.CreateVector(0.2, 0.2, 0.2);
-    if (!params.hasOwnProperty('buttonAlphaDisabled')) params.buttonAlphaDisabled = 0.5;
-    if (!params.hasOwnProperty('buttonColorPressed')) params.buttonColorPressed = mod.CreateVector(0.25, 0.25, 0.25);
-    if (!params.hasOwnProperty('buttonAlphaPressed')) params.buttonAlphaPressed = 1;
-    if (!params.hasOwnProperty('buttonColorHover')) params.buttonColorHover = mod.CreateVector(1, 1, 1);
-    if (!params.hasOwnProperty('buttonAlphaHover')) params.buttonAlphaHover = 1;
-    if (!params.hasOwnProperty('buttonColorFocused')) params.buttonColorFocused = mod.CreateVector(1, 1, 1);
-    if (!params.hasOwnProperty('buttonAlphaFocused')) params.buttonAlphaFocused = 1;
-}
-
-function __addUIButton(params: UIParams) {
-    __fillInDefaultArgs(params);
-    __fillInDefaultButtonArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIButton(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.buttonEnabled,
-            __asModVector(params.buttonColorBase),
-            params.buttonAlphaBase,
-            __asModVector(params.buttonColorDisabled),
-            params.buttonAlphaDisabled,
-            __asModVector(params.buttonColorPressed),
-            params.buttonAlphaPressed,
-            __asModVector(params.buttonColorHover),
-            params.buttonAlphaHover,
-            __asModVector(params.buttonColorFocused),
-            params.buttonAlphaFocused,
-            restrict
-        );
-    } else {
-        mod.AddUIButton(
-            __cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.buttonEnabled,
-            __asModVector(params.buttonColorBase),
-            params.buttonAlphaBase,
-            __asModVector(params.buttonColorDisabled),
-            params.buttonAlphaDisabled,
-            __asModVector(params.buttonColorPressed),
-            params.buttonAlphaPressed,
-            __asModVector(params.buttonColorHover),
-            params.buttonAlphaHover,
-            __asModVector(params.buttonColorFocused),
-            params.buttonAlphaFocused
-        );
-    }
-    return __setNameAndGetWidget(__cUniqueName, params);
-}
-
-function __addUIWidget(params: UIParams) {
-    if (params == null) return undefined;
-    if (params.type == 'Container') return __addUIContainer(params);
-    else if (params.type == 'Text') return __addUIText(params);
-    else if (params.type == 'Image') return __addUIImage(params);
-    else if (params.type == 'Button') return __addUIButton(params);
-    return undefined;
-}
-
-function ParseUI(...params: any[]) {
-    let widget: mod.UIWidget | undefined;
-    for (let a = 0; a < params.length; a++) {
-        widget = __addUIWidget(params[a] as UIParams);
-    }
-    return widget;
 }
